761fcaed306d9daf72fc1f9557aafe3519903129
Diego Santivañez <Diegojsf17@gmail.com>
2025-07-07 18:22:26 -0500

    feat: front v3

--- src/utils/axiosConfig.js
+++ src/utils/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:8085/',
+    baseURL: 'https://casafinderapi.mangograss-fe7e2f05.brazilsouth.azurecontainerapps.io/',
     headers: {
         'Content-Type': 'application/json',
     },
